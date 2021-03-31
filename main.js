c058491519713f71774a73ad0da628f530f2f692
alaa521 <alaaahmedamer521@gmail.com>
2021-04-01 01:15:17 +0300

    fetched api Relates #4

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const btn=document.getElementsByClassName('search-button')
+const btn=document.querySelector('.search-button')
 const img_result=document.querySelector('.box-result')
 btn.addEventListener('click',function get_dog(element){
     element.preventDefault();
@@ -6,31 +6,17 @@ document.querySelectorAll('.box-result img').forEach(function(item){
      item.remove()})
 
 const input_id=document.getElementsByClassName('search-engine').value;
-const url_api=`https://dog.ceo/api/breed/${input_id}/images/random&limit=1`;
-fetch(url_api)
+const link_api=`https://dog.ceo/api/breed/${input_id}/images/random&limit=1`;
+fetch(link_api)
 .then(res => {
     return res.json(); 
  })
  .then(data => {
     const dog_img=document.createElement('img')
-      film_img.alt='Film poster' 
-      img_result.appendChild(dog_img) 
-
-      .catch(err =>  console.log('err',err));
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    dog_img.src=`https://dog.ceo/api/breed/hound/images${data.poster_path}`
+       dog_img.alt='Dog poster'
+       img_result.appendChild(dog_img) 
+    })
+      .catch(err =>  console.log('error',erorr));
 
 })
